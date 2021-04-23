12985dfa65b127c5d3484b585c39e2090bc21dfb
Lauri Rooden <lauri@rooden.ee>
2021-04-23 21:58:55 +0300

    My conf

--- config.mk
+++ config.mk
@@ -4,8 +4,9 @@ VERSION = 0.9
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+DESTDIR = /home/lauri
+#PREFIX = /usr/local
+#MANPREFIX = $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
